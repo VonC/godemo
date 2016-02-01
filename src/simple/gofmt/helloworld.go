433d28752a3673788e47986eeac314e78220b37a
VonC <vonc@laposte.net>
2016-02-01 14:49:56 +0100

    restore simple gofmt example

--- src/simple/gofmt/helloworld.go
+++ src/simple/gofmt/helloworld.go
@@ -3,11 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	HelloWorld("in go")
-}
-
-// HelloWorld greets you
-// what specifies an additional message
-func HelloWorld(what string) {
-	fmt.Print("Hello World %s\n", what)
+	fmt.Println("Hello World in Go")
 }
